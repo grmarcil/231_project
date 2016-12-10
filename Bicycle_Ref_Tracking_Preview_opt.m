b5928754d6c71fcd5cf50b5ab9fbfc0a52439cee
Monimoy Bujarbaruah <monimoy.07@gmail.com>
2016-12-09 23:14:30 -0800

    Constraints commented out. They are causing infeasibility.

--- Bicycle_Ref_Tracking_Preview_opt.m
+++ Bicycle_Ref_Tracking_Preview_opt.m
@@ -15,9 +15,9 @@ ny = 4;      % Number of outputs
 
 %% MPC data
 C = eye(4); 
-Q = [10 0 0 0; 0 10 0 0; 0 0 0 0; 0 0 0 10];
+Q = 10*eye(4); 
 P = Q;
-R = eye(2);
+R = 5*eye(2);
 N = 6;
 
 %% Defining Variables
@@ -44,40 +44,46 @@ for k = 1:N
 
     
     
-    constru = [u{k}(2)<=0.6; -u{k}(2)<=0.6];
-    constru = [constru; u{k}(1)<=1.5*TS; -u{k}(1)<=1.5*TS];
-    constrx = [x{k}(1)<=20; -x{k}(1)<=20]; 
-    constrx = [constrx; x{k}(2)<=10; -x{k}(2)<=5];
-    constrx = [constrx; x{k}(3)<=10; -x{k}(3)<=10];
-    constrx = [constrx; x{k}(4)<=2*pi; -x{k}(4)<=2*pi];
-    
-    construx = [constru, constrx];
-   
-    
+%     constru = [u{k}(2)<=0.6; -u{k}(2)<=0.6];
+%     constru = [constru; u{k}(1)<=1.5*TS; -u{k}(1)<=1.5*TS];
+%     constrx = [x{k}(1)<=20; -x{k}(1)<=20]; 
+%     constrx = [constrx; x{k}(2)<=10; -x{k}(2)<=5];
+%     constrx = [constrx; x{k}(3)<=10; -x{k}(3)<=10];
+%     constrx = [constrx; x{k}(4)<=2*pi; -x{k}(4)<=2*pi];
+%     
+%     construx = [constru, constrx];
+%     
+%     
     constraints = [constraints, constr];
-    constraints = [constraints, construx];
+%   constraints = [constraints, construx];
+
 end
 
 objective = objective + (x{N+1}-xref{N+1})'*P*(x{N+1}-xref{N+1});
 
-%% Initializing  Control Desing
-
-xk = [0;0;0;0];
-xclloop(:,1) = xk;
-umpc_closedloop=[];
-ref=[];
-ym=[];
-Lsim = 150; % Length of simulation
 
 %% Constructing reference signal
 
-for i = 1:Lsim+N+1
-    if i<40
-        ref=[ref,zeros(4,1)];
-    else
-        ref=[ref,[10; 0 ; 1 ; 0]];
-    end
-end
+% for i = 1:Lsim+N+1
+%     if i<40
+%         ref=[ref,zeros(4,1)];
+%     else
+%         ref=[ref,[10; 0 ; 1 ; 0]];
+%     end
+% end
+
+[ref_x,ref_y,ref_v,ref_psi] = Path_Generation(); 
+ref = [ref_x;ref_y;ref_v;ref_psi];
+
+
+%% Initializing  Control Desing
+
+xk = ref(:,1);
+xclloop(:,1) = xk;
+umpc_closedloop=[];
+Lsim = 150; % Length of simulation
+
+
 
 %% Simulating MPC controller
 
@@ -95,7 +101,7 @@ for i = 1:Lsim
 for l=1:length(future_r)
     
     ur = sdpvar(2,N);
-    assign(ur,sqrt(0.1)*randn(2,N));
+    assign(ur,zeros(2,N));
   
 
         
@@ -105,9 +111,9 @@ for l=1:length(future_r)
     term4 = future_r(4,(2:end))-future_r(4,(1:end-1))-(future_r(3,(1:end-1))/lr.*sin(ur(2,(1:end))).*TS);
   
       
-%       cost = norm(ur)^2;
+%     cost = norm(ur)^2;
 
-      cost = norm(ur)^2 + norm(term1)^2+norm(term2)^2+norm(term3)^2+norm(term4)^2;
+      cost = norm(ur)^2 + 50*(norm(term1)^2+norm(term2)^2+norm(term3)^2+norm(term4)^2);
     
 
 %     constr1 =  ur(2,(1:end)).*TS == (future_r(1,(2:end))-future_r(1,(1:end-1))-future_r(3,(1:end-1)).*cos(future_r(4,(1:end-1))));
@@ -116,16 +122,16 @@ for l=1:length(future_r)
 %     constr1 = [constr1; future_r(4,(2:end))-future_r(4,(1:end-1))== future_r(3,(1:end-1))/lr.*sin(ur(2,(1:end))).*TS];
 
 
-    constr3 = [ur(2,:)<=0.6; -ur(2,:)<=0.6];
-    constr3 = [constr3; ur(1,:)<=1.5*TS; -ur(1,:)<=1.5*TS];
-
-%     constr13 = [constr1,constr3];
- 
-      constr13 = constr3;
+%     constr3 = [ur(2,:)<=0.6; -ur(2,:)<=0.6];
+%     constr3 = [constr3; ur(1,:)<=1.5*TS; -ur(1,:)<=1.5*TS];
+% 
+% %     constr13 = [constr1,constr3];
+%  
+%     constr13 = constr3;
+%     
+    options = sdpsettings('solver','ipopt','verbose',1,'usex',0);
     
-    options = sdpsettings('solver','fmincon','verbose',1,'usex',0);
-    
-    optimize(constr13,cost,options);
+    optimize([],cost,options);
     
     
     urr=double(ur);
@@ -167,7 +173,6 @@ end
 
     %% Plant model update
    
-       
     xk(1) = xk(1)+xk(3).*cos(xk(4)+U(2)).*TS;
     xk(2) = xk(2)+xk(3).*sin(xk(4)+U(2)).*TS;
     xk(3) = xk(3)+U(1).*TS;
@@ -176,8 +181,11 @@ end
     xk = [xk(1) xk(2) xk(3) xk(4)]';
     
     xclloop(:,i+1) = xk; 
+   
+    counter = i
     
 end
+
 
 %% Plot Results
 figure;
@@ -206,11 +214,6 @@ plot(xclloop(4,:)); grid on
 hold on;
 plot(xrefk_clloop(4,:),'r'); grid on
 legend(' \psi Closed loop actual','Reference');
-
-figure;
-plot(xclloop(1,:),xclloop(2,:)); 
-
-
 
 %% 
 % OL = Open Loop
