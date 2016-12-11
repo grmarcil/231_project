5392eee3c0115dda3badf1cf8c76905e26093c09
Greg Marcil <grmarcil@gmail.com>
2016-12-10 22:59:40 -0800

    Update Path_Generation for new coordinates

--- Path_Generation.m
+++ Path_Generation.m
@@ -5,6 +5,7 @@
 function [curve_x, curve_y, v , curve_psi ] = Path_Generation()
 %% Dimensions (all sizes in meters)
 lane_width = 3;
+lane_length = 10;
 
 car_width = 1.85;
 car_length = 4.9;
@@ -14,17 +15,10 @@ turn_radius = 5.56;
 %% Path generation
 % points for a right hand turn
 travel = 5;
-waypoints = [[0; 0], [0; travel - car_length/2], [0; travel + lane_width/2], [lane_width/2 + car_length/2; travel + lane_width/2], [lane_width/2 + travel; travel + lane_width/2]];
-
-lane_lower = [[0 + lane_width/2; 0], [0 + lane_width/2; travel], [0 + lane_width/2; travel], [lane_width/2; travel], [lane_width/2 + travel; travel]];
-lane_upper = [[0 - lane_width/2; 0], [0 - lane_width/2; travel], [0 - lane_width/2; travel + lane_width], [lane_width/2; travel + lane_width], [lane_width/2 + travel; travel + lane_width]];
-% waypoints = [[0; 0], [0; 20 - car_length/2], [0; 20 + lane_width/2], [lane_width/2 + car_length/2; 20 + lane_width/2], [lane_width/2 + car_length/2 + 20; 20 + lane_width/2]];
-
-% x_path = waypoints(1,:);
-% y_path = waypoints(2,:);
-% li_pts = 10; % number of straight line interpolation points
-% x_path = [linspace(waypoints(1,1),waypoints(1,2),li_pts), linspace(waypoints(1,3), waypoints(1,4),li_pts)];
-% y_path = [linspace(waypoints(2,1),waypoints(2,2),li_pts), linspace(waypoints(2,3), waypoints(2,4),li_pts)];
+% shorthand vars
+lw = lane_width;
+ll = lane_length;
+waypoints = [[lw/2; -ll-lw], [lw/2;  -lw-car_length/2], [lw/2; -lw/2], [lw + car_length/2; -lw/2], [lw+ll;  -lw/2]];
 
 li_pts = 10; % number of straight line interpolation points
 x_path = [linspace(waypoints(1,1),waypoints(1,2),li_pts), waypoints(1,3), linspace(waypoints(1,4), waypoints(1,5),li_pts)];
@@ -40,34 +34,35 @@ plot(waypoints(1,:), waypoints(2,:), 'xb');
 curve_values = spcrv([x_path; y_path]);
 plot(curve_values(1,:),curve_values(2,:),'r','LineWidth',1.5);
 
-% plot lanes
-plot(lane_lower(1,:), lane_lower(2,:), 'k', 'LineWidth', 1.5);
-plot(lane_upper(1,:), lane_upper(2,:), 'k', 'LineWidth', 1.5);
-
 legend({'Waypoints' 'Fitted Spline Curve'},  'location','SE');
 hold off
 
 %% State Reference Generation
 
-curve_x = curve_values(1,:);
-curve_y = curve_values(2,:);
+curve.x = curve_values(1,:);
+curve.y = curve_values(2,:);
 
-% Precompute heading angles along curve
-curve_psi = [];
-for i = 1:(length(curve_x)-1)
-    dx = curve_x(i+1) - curve_x(i);
-    dy = curve_y(i+1) - curve_y(i);
-    curve_psi(i) = atan2(dy, dx);
+% Precompute heading angles and distances along curve
+curve.psi = [0];
+curve.dist = [0];
+for i = 2:length(curve.x)
+    dx = curve.x(i) - curve.x(i-1);
+    dy = curve.y(i) - curve.y(i-1);
+    curve.psi(i) = atan2(dy, dx);
+    curve.dist(i) = curve.dist(i-1) + sqrt(dx^2 + dy^2);
 end
-curve_psi(end) = curve_psi(end-1); % assume the same heading for final step
+curve.psi(1) = curve.psi(2); % assume the same angle for first and second steps
 
-% Precompute distances along curve
-curve_dist = [0];
-for i = 2:length(curve_x)
-    dx = curve_x(i) - curve_x(i-1);
-    dy = curve_y(i) - curve_y(i-1);
-    curve_dist(i) = curve_dist(i-1) + sqrt(dx^2 + dy^2);
-end
+% Enforce even spacing of all points on reference curve
+new_dist = linspace(curve.dist(1), curve.dist(end), length(curve.dist));
+new_x = interp1(curve.dist, curve.x, new_dist);
+new_y = interp1(curve.dist, curve.y, new_dist);
+new_psi = interp1(curve.dist, curve.psi, new_dist);
+curve = struct('dist', new_dist, 'x', new_x, 'y', new_y, 'psi', new_psi);
+% Add interpolation functions to curve struct
+curve.interp_x = @(dist) interp1(curve.dist, curve.x, dist);
+curve.interp_y = @(dist) interp1(curve.dist, curve.y, dist);
+curve.interp_psi = @(dist) interp1(curve.dist, curve.psi, dist);
 
 % Problem parameters
 % Assuming constant v for now
@@ -75,92 +70,10 @@ v = 4.5; % m/s (~10 mph)
 dt = 0.2;
 N = 6;
 
-% To be run at each time step before solving MPC problem
-
-% Need a routine here to project the current state to find nearest path point
-% Algorithm sketch
-% 1. Begin at start point with no error
-% 2. Compute N reference steps and exectue time step: x1, ..., xN
-% 3. Take current state x(t) and assume the closest path point is last x1
-% 4. Examine path points before and after x1. Follow direction in which distance
-% between x(t) and path point decreases until it starts increasing. This assumes
-% the path has decent local convexity, which is probably fine but might fail for
-% e.g. parallel parking
-
-% Initial state (x,y,v,psi)
-% Assuming for now we projected onto the path
-% Reset this at each time step
-x0 = [0;0;v;pi/2];
-% Find what point index you're at in the curve
-idx = 1;
-cur_dist = curve_dist(idx);
-z_ref = [x0];
-
-temp_idx = idx;
-for i=1:N
-    dist = cur_dist + i*v*dt;
-    % find closest point in curve_dist to dist
-    % or just interpolate path to have constant d(dist) between points
-    check_dist = curve_dist(temp_idx);
-    while(check_dist < dist)
-        temp_idx = temp_idx + 1;
-        check_dist = curve_dist(temp_idx);
-    end
-    z_ref = [z_ref, [curve_x(temp_idx); curve_y(temp_idx); v; curve_psi(temp_idx)]];
-end
-
-
+curve_psi = curve.psi;
 v = v*ones(1,length(curve_psi)); 
-curve_x = curve_x(1,1:end-1);
-curve_y = curve_y(1,1:end-1); 
+curve_x = curve.x;
+curve_y = curve.x; 
 end
 
 
-%% Input Reference Generation
-
-%% Leftover experimental code
-
-% %% Manual parametric splines
-% 
-% time = 1:length(x_path);
-% knots = 1:0.5:length(x_path);
-% 
-% % xspline = csapi(time,x_path);
-% % yspline = csapi(time,y_path);
-% xspline = spapi(3, time, x_path);
-% yspline = spapi(3, time, y_path);
-% fnplt(xspline);
-% 
-% % x_points = ppval(xspline, 1:0.1:length(x_path));
-% % y_points = ppval(yspline, 1:0.1:length(y_path));
-% 
-% xmids = [];
-% ymids = [];
-% for i=1:length(x_path)-1
-%     xmids(i) = (x_path(i) + x_path(i+1))/2;
-%     ymids(i) = (y_path(i) + y_path(i+1))/2;
-% end
-% % plot(x_points, y_points);
-% time2 = 1:length(xmids);
-% xspline2 = spapi(3,time2,xmids);
-% yspline2 = spapi(3,time2,ymids);
-% fnplt(xspline2);
-% hold on; fnplt(xspline, 'g'); hold off;
-% 
-% x_points = fnval(xspline2, 1:0.1:length(time2));
-% y_points = fnval(yspline2, 1:0.1:length(time2));
-% plot(x_points,y_points);
-% 
-% %%
-% xpts = values(1,:);
-% ypts = values(2,:);
-% time = 1:length(xpts);
-% 
-% xspline = spapi(3,time,xpts);
-% yspline = spapi(3,time,ypts);
-% fnplt(xspline);
-% hold on; plot(time,xpts, 'g'); hold off;
-% %%
-% x_points = fnval(xspline2, 1:0.1:length(time2));
-% y_points = fnval(yspline2, 1:0.1:length(time2));
-% plot(x_points,y_points);
