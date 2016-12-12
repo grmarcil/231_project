3a53d3bcbc963393d9b190139b513c5605fe8bd6
Monimoy Bujarbaruah <monimoy.07@gmail.com>
2016-12-12 02:37:30 -0800

    Stop at the intersection
    Stopping at the intersection coded. However for stopping, acceleration
    constraint is clearly violated. Need to adapt the syntax in Julia as
    the optimization isn’t working on MATLAB.

--- Bicycle_Final_Matlab.m
+++ Bicycle_Final_Matlab.m
@@ -177,38 +177,38 @@ animation = Plot_Simulation(z_cl, u_cl, lane_width, lane_length, path);
 
 
 %% Plot Results
-% figure;
-% plot(z_cl(1,:)); grid on
-% hold on;
-% plot(z_ref_cl(1,:),'r'); grid on
-% legend(' x Closed loop actual','Reference');
-%
-% figure;
-% plot(z_cl(2,:)); grid on
-% hold on;
-% plot(z_ref_cl(2,:),'r'); grid on
-% legend(' y Closed loop actual','Reference');
+figure;
+plot(z_cl(1,:)); grid on
+hold on;
+plot(z_ref_cl(1,:),'r'); grid on
+legend(' x Closed loop actual','Reference');
 
-% figure;
-% plot(z_cl(3,:)); grid on
-% hold on;
-% plot(z_ref_cl(3,:),'r'); grid on
-% legend(' v Closed loop actual','Reference');
+figure;
+plot(z_cl(2,:)); grid on
+hold on;
+plot(z_ref_cl(2,:),'r'); grid on
+legend(' y Closed loop actual','Reference');
 
-% figure;
-% plot(z_cl(4,:)); grid on
-% hold on;
-% plot(z_ref_cl(4,:),'r'); grid on
-% legend(' \psi Closed loop actual','Reference');
-%
-% figure;
-% plot(u_cl(1,:)); grid on
-% hold on;
-% plot(u_ref_cl(1,:),'r'); grid on
-% legend(' acceleration Closed loop actual','Reference');
-%
-% figure;
-% plot(u_cl(2,:)); grid on
-% hold on;
-% plot(u_ref_cl(2,:),'r'); grid on
-% legend(' \beta Closed loop actual','Reference');
+figure;
+plot(z_cl(3,:)); grid on
+hold on;
+plot(z_ref_cl(3,:),'r'); grid on
+legend(' v Closed loop actual','Reference');
+
+figure;
+plot(z_cl(4,:)); grid on
+hold on;
+plot(z_ref_cl(4,:),'r'); grid on
+legend(' \psi Closed loop actual','Reference');
+
+figure;
+plot(u_cl(1,:)); grid on
+hold on;
+plot(u_ref_cl(1,:),'r'); grid on
+legend(' acceleration Closed loop actual','Reference');
+
+figure;
+plot(u_cl(2,:)); grid on
+hold on;
+plot(u_ref_cl(2,:),'r'); grid on
+legend(' \beta Closed loop actual','Reference');
